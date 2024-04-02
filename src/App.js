8f2f1a5896055838b6ee519065cd1540b0b20ee0
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-04-02 11:44:14 +1100

    react 25 projects

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Accordian from "./components/accordian";
+// high order component
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Accordian />
+      </div>
+    </>
+  );
+}
+
+export default App;
