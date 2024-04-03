58ecbe51e69dc386f18a67789887c28bc56cf6d3
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-04-03 15:09:16 +1100

    Added tree view structure project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import TreeMenu from "./components/tree-view";
+import menus from "./components/tree-view/data";
 // high order component
 
 function App() {
   return (
     <>
-      {/**/} {/**/} {/**/} {/**/} {/**/} {/**/}
+      {/**/} {/**/} {/**/} {/**/} {/**/}
       {/*   <Accordian />*/}
       {/* <RandomColor />*/}
       {/* <StarRating noOfStar={10} />*/}
@@ -18,7 +20,8 @@ function App() {
         page={"1"}
         limit={"10"}
       />*/}
-      <LoadMoreData />
+      {/*<LoadMoreData />*/}
+      <TreeMenu menu={menus} />
     </>
   );
 }
