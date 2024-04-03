aebdecf5de19442d892e8f125836152840a1885a
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-04-03 11:58:41 +1100

    Added image-slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 // high order component
@@ -7,12 +8,15 @@ import StarRating from "./components/star-rating";
 function App() {
   return (
     <>
-      <div className="App">
-        {/**/} {/**/} {/**/} {/**/} {/**/} {/**/} {/**/}
-        {/*   <Accordian />*/}
-        {/* <RandomColor />*/}
-        <StarRating noOfStar={10} />
-      </div>
+      {/**/} {/**/} {/**/} {/**/} {/**/} {/**/}
+      {/*   <Accordian />*/}
+      {/* <RandomColor />*/}
+      {/* <StarRating noOfStar={10} />*/}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
